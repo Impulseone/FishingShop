e245495b19ec2f31830602a9b53ca80a45378b83
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-02-07 20:43:12 +0300

    init

---
+++ app/src/main/java/com/skynet/fishingshop/view/authorization/AuthorizationInitActivity.java
@@ -0,0 +1,16 @@
+package com.skynet.fishingshop.view.authorization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.skynet.fishingshop.R;
+
+public class AuthorizationInitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization_init);
+    }
+}
