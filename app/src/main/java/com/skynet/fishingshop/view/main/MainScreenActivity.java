e1ae1bc62d1aa55dac1c35282db6a164a8bc1f97
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-02-15 11:22:26 +0300

    navigation drawer created

--- app/src/main/java/com/skynet/fishingshop/view/main/MainScreenActivity.java
+++ app/src/main/java/com/skynet/fishingshop/view/main/MainScreenActivity.java
@@ -1,9 +1,20 @@
 package com.skynet.fishingshop.view.main;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,12 +25,38 @@ import com.skynet.fishingshop.view.main.favorites.FavoritesFragment;
 
 public class MainScreenActivity extends AppCompatActivity {
 
+    private String[] mScreenTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSupportActionBar((Toolbar)findViewById(R.id.tool_bar));
+        mScreenTitles = getResources().getStringArray(R.array.screen_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+                R.layout.drawer_list_item, mScreenTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         createHomeFragment();
 
@@ -40,16 +77,9 @@ public class MainScreenActivity extends AppCompatActivity {
         });
     }
 
-//    private void createHomeFragment() {
-//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//        HomeFragment homeFragment = new HomeFragment();
-//        ft.replace(R.id.main_relative_layout, homeFragment);
-//        ft.commit();
-//    }
-
     private void createHomeFragment() {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        FavoritesFragment homeFragment = new FavoritesFragment();
+        HomeFragment homeFragment = new HomeFragment();
         ft.replace(R.id.main_relative_layout, homeFragment);
         ft.commit();
     }
@@ -65,6 +95,58 @@ public class MainScreenActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.main_relative_layout, new CartFragment());
         ft.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new ProfileFragment();
+                break;
+            case 1:
+                fragment = new FavoritesFragment();
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_relative_layout, fragment).commit();
+            mDrawerList.setItemChecked(position, true);
+            mDrawerLayout.closeDrawer(mDrawerList);
+        } else {
+            Log.e(this.getClass().getName(), "Error. Fragment is not created");
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
 }
